7aba9dd5f2927711d9e9e28cb5c872703f6477ce
Fernando Rocha <fhsrocha@gmail.com>
2017-09-18 15:20:35 -0300

    envio de email
    formatação da mansegem de envio

--- src/static/enviar.php
+++ src/static/enviar.php
@@ -11,10 +11,9 @@ $nomeremetente     = $_POST['nome'];
 $emailremetente    = $_POST['email'];
 $assunto           = $_POST['assunto'];
 $emaildestinatario = 'secretaria@lemaf.ufla.br';
-$mensagemHTML      = 'De: '.$nomeremetente.'<br/> E-mail: '.$emailremetente.'<br/><br/> Mensagem: '.$_POST['mensagem'];
+$mensagemHTML      = '<b>De:</b> '.$nomeremetente.'<br/> <b>E-mail:</b> '.$emailremetente.'<br/><br/> <b>Mensagem:</b> '.$_POST['mensagem'];
 
 /* Montando o cabeçalho da mensagem */
-
 $headers = "MIME-Version: 1.1" .$quebra_linha;
 $headers .= "Content-type: text/html; charset=iso-8859-1" .$quebra_linha;
 
@@ -22,7 +21,6 @@ $headers .= "From: " . $emailsender . $quebra_linha;
 $headers .= "Reply-To: " . $emailremetente . $quebra_linha;
 
 /* Enviando a mensagem */
-
 if(!mail($emaildestinatario, $assunto, $mensagemHTML, $headers ,"-r".$emailsender)){
   $headers .= "Return-Path: " . $emailsender . $quebra_linha;
   mail($emaildestinatario, $assunto, $mensagemHTML, $headers );
